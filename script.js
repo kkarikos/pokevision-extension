f59cf42efa454d9b8d1cee41dc7ed001279ecdfb
Kalle Karikoski <kalle.karikoski@vincit.fi>
2016-07-28 11:14:26 +0300

    chore: Cleanup

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 var s = document.createElement('script');
-// TODO: add "script.js" to web_accessible_resources in manifest.json
 s.src = chrome.extension.getURL('main.js');
 s.onload = function() {
     this.remove();
